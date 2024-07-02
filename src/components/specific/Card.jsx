69575087ebae4f0ee13b284b9727bfabeb4fb2bb
Yash Mahalle <yash65mahalle@gmail.com>
2024-07-02 16:41:27 +0530

    changes in tooltip

--- src/components/specific/Card.jsx
+++ src/components/specific/Card.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+// src/components/Card.js
+import React from "react";
 import { FaEdit, FaEye, FaEyeSlash, FaTrash } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Tooltip from "react-tooltip-lite";
+import ReactTooltip from "react-tooltip";
 import { deleteMovie, toggleWatched } from "../../store/slice/movieSlice";
-import DeleteDialog from "./DeleteDialog";
 
 const Card = ({ movie }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [showPrompt, setShowPrompt] = useState(false);
 
   const handleToggleWatched = (e) => {
     e.stopPropagation();
@@ -22,13 +21,12 @@ const Card = ({ movie }) => {
 
   const handleDelete = (e) => {
     e.stopPropagation();
-    setShowPrompt(true); 
-  };
-
-  const handleConfirmDelete = () => {
     dispatch(deleteMovie(movie.id));
     toast.error("Movie deleted successfully");
-    setShowPrompt(false); 
+  };
+
+  const handleCardClick = () => {
+    navigate(`/details/${movie.id}`);
   };
 
   const handleEdit = (e) => {
@@ -36,13 +34,12 @@ const Card = ({ movie }) => {
     navigate(`/edit/${movie.id}`);
   };
 
-  const handleCardClick = () => {
-    navigate(`/details/${movie.id}`);
-  };
-
   return (
-    <div className="bg-white p-4 rounded shadow-lg relative  flex justify-between">
-      <div onClick={handleCardClick} className="cursor-pointer w-full">
+    <div
+      onClick={handleCardClick}
+      className="bg-white p-4 rounded shadow-lg relative flex items-center justify-between hover:bg-gray-100 cursor-pointer"
+    >
+      <div>
         <h2 className="text-xl font-bold">{movie.title}</h2>
         <p className="text-gray-500">Genre: {movie.genre}</p>
         <div className="flex items-center">
@@ -63,41 +60,31 @@ const Card = ({ movie }) => {
       </div>
 
       <div className="flex items-center gap-4">
-        <Tooltip content={movie.watched ? "UnWatch" : "watch"}>
-          <div
-            onClick={handleToggleWatched}
-            className="text-green-500 hover:text-green-700 cursor-pointer"
-          >
-            {movie.watched ? <FaEyeSlash /> : <FaEye />}
-          </div>
-        </Tooltip>
+        <button
+          data-tip={movie.watched ? "Unwatch" : "Watch"}
+          className="text-green-500 hover:text-green-700"
+          onClick={handleToggleWatched}
+        >
+          {movie.watched ? <FaEyeSlash /> : <FaEye />}
+        </button>
 
-        <Tooltip content="Edit">
-          <div
-            onClick={handleEdit}
-            className="text-blue-500 hover:text-blue-700 cursor-pointer"
-          >
-            <FaEdit />
-          </div>
-        </Tooltip>
+        <button
+          data-tip="Edit"
+          className="text-blue-500 hover:text-blue-700"
+          onClick={handleEdit}
+        >
+          <FaEdit />
+        </button>
 
-        <Tooltip content="Delete">
-          <div
-            onClick={handleDelete}
-            className="text-red-500 hover:text-red-700 cursor-pointer"
-          >
-            <FaTrash />
-          </div>
-        </Tooltip>
+        <button
+          data-tip="Delete"
+          onClick={handleDelete}
+          className="text-red-500 hover:text-red-700 cursor-pointer"
+        >
+          <FaTrash />
+        </button>
+        <ReactTooltip />
       </div>
-
-      {showPrompt && (
-        <DeleteDialog
-          message={`Do you really want to delete the "${movie.title}" movie?`}
-          onConfirm={handleConfirmDelete}
-          onCancel={() => setShowPrompt(false)}
-        />
-      )}
     </div>
   );
 };
